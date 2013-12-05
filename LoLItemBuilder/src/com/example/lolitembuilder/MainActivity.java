5ab1ad4cf2a8b4a0c62535313043a181743fe917
Scott-E <scott.elliott65@gmail.com>
2013-12-05 11:30:19 -0500

    Modified main layout

--- LoLItemBuilder/src/com/example/lolitembuilder/MainActivity.java
+++ LoLItemBuilder/src/com/example/lolitembuilder/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lolitembuilder;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
